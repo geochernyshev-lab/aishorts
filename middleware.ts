6dcdf6bbdd58dabba2b191b070f200e77e52be49
geochernyshev-lab <geochernyshev@gmail.com>
2026-03-04 01:22:58 -0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/webhooks(.*)",
+  "/api/poll(.*)",
 ]);
 
 export default clerkMiddleware((auth, request) => {
